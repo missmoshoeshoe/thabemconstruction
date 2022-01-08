a17337ec949e63fa0f1eaf1c1fd893f2ebf0c714
Mamello <mmoshoeshoe@webelieve.co.za>
2022-01-08 23:09:46 +0200

    add basic header to gallery and quotation page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,16 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
 import { AboutPageComponent } from './pages/about-page/about-page.component';
 import { ServicesPageComponent } from './pages/services-page/services-page.component';
 import { ContactPageComponent } from './pages/contact-page/contact-page.component';
+import { QuotationPageComponent } from './pages/quotation-page/quotation-page.component';
+import { GalleryPageComponent } from './pages/gallery-page/gallery-page.component';
 
 const routes: Routes = [
   { path: '', component: HomePageComponent },
   { path: 'about', component: AboutPageComponent},
   { path: 'services', component: ServicesPageComponent},
-  { path: 'contact', component: ContactPageComponent}
+  { path: 'contact', component: ContactPageComponent},
+  { path: 'quotation', component: QuotationPageComponent},
+  { path: 'gallery', component: GalleryPageComponent}
 ];
 
 @NgModule({
